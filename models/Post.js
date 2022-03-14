e43d5ef37bab539f8fceddb2418815bf5cb5b2e8
kartowka <antonfley@gmail.com>
2022-03-14 14:27:53 +0200

    added post model,student model for mongoDB scheme

---
+++ models/Post.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+const postSchema = new mongoose.Schema({
+	message: {
+		type: String,
+		required: true,
+	},
+	sender: {
+		type: String,
+		required: true,
+	},
+})
+
+export default mongoose.model('Post', postSchema)
