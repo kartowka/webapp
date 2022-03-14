27cd0927c2fa30b85320e1a3471b6aad17e3a152
kartowka <antonfley@gmail.com>
2022-03-14 14:24:53 +0200

    added middleware,bodyparser,dbconnection,env variables added

--- server.js
+++ server.js
@@ -1,22 +1,44 @@
+//* application
+
 import express from 'express'
 const app = express()
 
-//? environment files
+//* environment files
+
 import dotenv from 'dotenv'
 dotenv.config()
 
-//?routers
-import postRouter from './routes/post_routes.js'
+//* db
 
+import connectDB from './db/connect.js'
+
+//* routers
+
+import postRouter from './routes/post.js'
+
+//* middleware
+
+import not_found_middleware from './middleware/not_found.js'
+
+//* app.use
+import bodyParser from 'body-parser'
+app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }))
+app.use(bodyParser.json())
 app.get('/', (req, res) => {
-	res.send('Hello world!')
+	res.json({ msg: 'Welcome!' })
 })
 
 app.use('/post', postRouter)
+app.use(not_found_middleware)
+
+//* server create
 
 const PORT = process.env.PORT || 3000
+
 const start = async () => {
 	try {
+		await connectDB(process.env.MONGO_URL)
+		console.log('connected to mongoDB')
 		app.listen(PORT, () => console.log(`server is listening on port ${PORT}`))
 	} catch (error) {
 		console.log(error)
