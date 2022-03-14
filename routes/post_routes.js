cb56dfcfa6b2bbf0c062b5c87034120ba1664ebf
kartowka <antonfley@gmail.com>
2022-03-14 14:23:39 +0200

    renamed file

--- routes/post_routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-const router = express.Router()
-import { getPosts, createNewPost } from '../controller/post.js'
-
-router.get('/', getPosts)
-
-router.post('/', createNewPost)
-
-export default router
