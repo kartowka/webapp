fabd3422714cec2896ae9588d521c29939c032e4
kartowka <antonfley@gmail.com>
2022-03-15 12:36:28 +0200

    added delete func

--- controllers/post.js
+++ controllers/post.js
@@ -1,15 +1,16 @@
 import Post from '../models/Post.js'
 import { StatusCodes } from 'http-status-codes'
+import BadRequestError from '../errors/bad-request.js'
 
 const getPostById = async (req, res) => {
-	if (req.params.id == null || req.params.id == undefined) {
-		return res.status(StatusCodes.BAD_REQUEST).send({ err: 'NO ID PROVIDED' })
-	}
 	try {
 		const post = await Post.findById(req.params.id)
+		if (post == null || post == undefined) {
+			throw new BadRequestError('ID not exist')
+		}
 		res.status(StatusCodes.OK).send(post)
 	} catch (err) {
-		res.status(StatusCodes.BAD_REQUEST).send({ err: err.message })
+		res.status(err.StatusCode).send({ err: err.message })
 	}
 }
 const getPosts = async (req, res) => {
@@ -38,5 +39,15 @@ const createNewPost = async (req, res) => {
 		res.status(StatusCodes.BAD_REQUEST).send(err.message)
 	}
 }
+const deletePostByID = async (req, res) => {
+	try {
+		await Post.deleteOne({ _id: req.params.id })
+		res.status(StatusCodes.OK).send()
+	} catch (err) {
+		res.status(StatusCodes.BAD_REQUEST).send({
+			err: err.message,
+		})
+	}
+}
 
-export { getPosts, createNewPost, getPostById }
+export { getPosts, createNewPost, getPostById, deletePostByID }
