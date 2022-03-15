79004099e20a700af8b62a0e407516aa956f0e26
kartowka <antonfley@gmail.com>
2022-03-15 12:36:47 +0200

    added delete route

--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,10 @@
 import express from 'express'
 const router = express.Router()
-import { getPosts, createNewPost, getPostById } from '../controllers/post.js'
+import { getPosts, createNewPost, getPostById, deletePostByID } from '../controllers/post.js'
 
 router.post('/', createNewPost)
 router.get('/', getPosts)
 router.get('/:id', getPostById)
+router.delete('/:id', deletePostByID)
 
 export default router
