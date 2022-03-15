c34c6b9a71cd8535735d89e35299bb13ba18c22a
kartowka <antonfley@gmail.com>
2022-03-15 21:01:58 +0200

    removed unused deletation and declaration

--- tests/post_routes.test.js
+++ tests/post_routes.test.js
@@ -8,36 +8,34 @@ const postMessage = 'this is my test post'
 const sender = 'Fleysher'
 var senderID = ''
 
-beforeAll(async () => {
-	await Post.deleteOne({ sender: sender })
-})
+beforeAll(() => {})
 afterAll(async () => {
 	mongoose.connection.close()
 })
 describe('Testing Post API', () => {
-	test('test function createPost', async () => {
+	test('function createPost', async () => {
 		const response = await request(app).post('/post').send({ message: postMessage, sender: sender })
 		expect(response.statusCode).toEqual(StatusCodes.OK)
 	})
-	test('test function getPosts', async () => {
+	test('function getPosts', async () => {
 		const response = await request(app).get('/post')
 		expect(response.statusCode).toEqual(StatusCodes.OK)
 	})
-	test('test function getPostsBySender', async () => {
+	test('function getPostsBySender', async () => {
 		const response = await request(app).get('/post?sender=' + sender)
 		senderID = response.body[0]._id
 		expect(response.statusCode).toEqual(StatusCodes.OK)
 		expect(response.body[0].message).toEqual(postMessage)
 		expect(response.body[0].sender).toEqual(sender)
 	})
-	test('test function getPostsById', async () => {
+	test('function getPostsById', async () => {
 		const response = await request(app).get('/post/' + senderID)
 		expect(response.statusCode).toEqual(StatusCodes.OK)
 		expect(response.body.message).toEqual(postMessage)
 		expect(response.body.sender).toEqual(sender)
 		expect(response.body._id).toEqual(senderID)
 	})
-	test('test function deletePostByID', async () => {
+	test('function deletePostByID', async () => {
 		const response = await request(app).delete('/post/' + senderID)
 		expect(response.statusCode).toEqual(StatusCodes.OK)
 		const userExistAfterDelete = await request(app).get('/post/' + senderID)
