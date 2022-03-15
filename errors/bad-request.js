8f44f3773d34f2859788657cf893262639fc4d0d
kartowka <antonfley@gmail.com>
2022-03-15 12:36:08 +0200

    added custom errors

---
+++ errors/bad-request.js
@@ -0,0 +1,12 @@
+import { StatusCodes } from 'http-status-codes'
+import CustomAPIError from './custom-api.js'
+
+StatusCodes
+class BadRequestError extends CustomAPIError {
+	constructor(message) {
+		super(message)
+		this.StatusCode = StatusCodes.BAD_REQUEST
+	}
+}
+
+export default BadRequestError
