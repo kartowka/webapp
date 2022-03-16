627486b0b53fb3b33b3a189d96462d53593ab0f6
kartowka <antonfley@gmail.com>
2022-03-16 11:22:36 +0200

    moved connection to db to custom file

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,11 @@
 import mongoose from 'mongoose'
 
-const connectDB = (url) => {
-	return mongoose.connect(url, { useNewUrlParser: true })
+const connectDB = async (url) => {
+	try {
+		await mongoose.connect(url, { useNewUrlParser: true })
+		console.log('connected to DB')
+	} catch (e) {
+		console.log(e)
+	}
 }
 export default connectDB
