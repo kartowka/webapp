f4cd2ad1b8659c95f40fa40c2ee0738c1977dbfd
kartowka <antonfley@gmail.com>
2022-03-16 11:18:10 +0200

    server now run for server file

--- server.js
+++ server.js
@@ -1,44 +1,10 @@
-//* application
-
-import express from 'express'
-const app = express()
-
-//* environment files
-
-import dotenv from 'dotenv'
-dotenv.config()
-
-//* routers
-
-import postRouter from './routes/post.js'
-
-//* middleware
-
-import not_found_middleware from './middleware/not_found.js'
-
-//* app.use
-import bodyParser from 'body-parser'
-app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }))
-app.use(bodyParser.json())
-app.get('/', (req, res) => {
-	res.json({ msg: 'Welcome!' })
-})
-
-app.use('/post', postRouter)
-app.use(not_found_middleware)
-
-//* db
-
-import connectDB from './db/connect.js'
-
-//* server create
-
+import app from './app.js'
 const PORT = process.env.PORT || 3000
 
 const start = async () => {
 	try {
-		await connectDB(process.env.MONGO_URL)
-		console.log('connected to mongoDB')
+		// await connectDB(process.env.MONGO_URL)
+		// console.log('connected to mongoDB')
 		app.listen(PORT, () => console.log(`server is listening on port ${PORT}`))
 	} catch (error) {
 		console.log(error)
@@ -46,5 +12,3 @@ const start = async () => {
 }
 
 start()
-
-export default app
