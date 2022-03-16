844e719e09db4533a2a4df7af235b5c2025eafe5
kartowka <antonfley@gmail.com>
2022-03-16 11:25:52 +0200

    changed syntax for routes

--- routes/post.js
+++ routes/post.js
@@ -2,9 +2,9 @@ import express from 'express'
 const router = express.Router()
 import { getPosts, createNewPost, getPostById, deletePostByID } from '../controllers/post.js'
 
-router.post('/', createNewPost)
-router.get('/', getPosts)
-router.get('/:id', getPostById)
-router.delete('/:id', deletePostByID)
+router.route('/').post(createNewPost)
+router.route('/').get(getPosts)
+router.route('/:id').get(getPostById)
+router.route('/:id').delete(deletePostByID)
 
 export default router
