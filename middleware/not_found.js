faae028cb9338baaa5e704848e93fe9642edaf50
kartowka <antonfley@gmail.com>
2022-03-17 14:44:31 +0200

    moved files to new dest as well converted to TypeScript

--- middleware/not_found.js
+++
@@ -1,6 +0,0 @@
-import { StatusCodes } from 'http-status-codes'
-
-const not_found_middleware = (req, res) => {
-	res.status(StatusCodes.NOT_FOUND).send('routes does not found')
-}
-export default not_found_middleware
--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-const router = express.Router()
-import { register, login, deleteByID } from '../controllers/auth.js'
-
-router.route('/register').post(register)
-router.route('/login').post(login)
-router.route('/user/:id').delete(deleteByID)
-
-export default router
