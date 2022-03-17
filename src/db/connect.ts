4b7d0811e07ecd26c559aa618f6593ec0f1f97c6
kartowka <antonfley@gmail.com>
2022-03-17 16:31:48 +0200

    added await to mongoose connect

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async (url: string) => {
 	try {
-		mongoose.connect(url)
+		await mongoose.connect(url)
 		// tslint:disable-next-line:no-console
 		console.log('connected to DB')
 	} catch (e) {
