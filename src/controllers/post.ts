c5bed05f3434210af6891159c54c9e6b5fc0d1f1
kartowka <antonfley@gmail.com>
2022-03-19 17:58:28 +0200

    removed unused ifs

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -3,42 +3,39 @@ import { StatusCodes } from 'http-status-codes'
 import BadRequestError from '../errors/bad-request.js'
 import { Request, Response } from 'express'
 
-const getPostById = async (req:Request, res:Response) => {
-	const post = await Post.findById(req.params.id)
-	if (post === null || post === undefined) {
-		throw new BadRequestError('ID not exist')
-	}
-	res.status(StatusCodes.OK).json(post)
+const getPostById = async (req: Request, res: Response) => {
+  const post = await Post.findById(req.params.id)
+  if (post === null || post === undefined) {
+    throw new BadRequestError('ID not exist')
+  }
+  res.status(StatusCodes.OK).json(post)
 }
-const getPosts = async (req:Request, res:Response) => {
-	let post
-	const sender = req.query.sender
-	if (sender != null || sender != undefined) {
-		// * GET POST BY SENDER
-		post = await Post.find({ sender })
-	} else {
-		// * GET ALL POSTS
-		post = await Post.find()
-	}
-	res.status(StatusCodes.OK).json(post)
+const getPosts = async (req: Request, res: Response) => {
+  let post
+  const sender = req.query.sender
+  if (sender != null || sender != undefined) {
+    // * GET POST BY SENDER
+    post = await Post.find({ sender })
+  } else {
+    // * GET ALL POSTS
+    post = await Post.find()
+  }
+  res.status(StatusCodes.OK).json(post)
 }
-const createNewPost = async (req:Request, res:Response) => {
-	const post = new Post({
-		message: req.body.message,
-		sender: req.body.sender,
-	})
-	const newPost = await post.save()
-	if (!newPost) {
-		throw new BadRequestError('something wrong!, check if message or sender exist')
-	}
-	res.status(StatusCodes.OK).json(post)
+const createNewPost = async (req: Request, res: Response) => {
+  const post = new Post({
+    message: req.body.message,
+    sender: req.body.sender,
+  })
+  const newPost = await post.save()
+  res.status(StatusCodes.OK).json(post)
 }
-const deletePostByID = async (req:Request, res:Response) => {
-	const postToDelete = await Post.deleteOne({ id: req.params.id })
-	if (!postToDelete) {
-		throw new BadRequestError('user not exist')
-	}
-	res.status(StatusCodes.OK).json()
+const deletePostByID = async (req: Request, res: Response) => {
+  const postToDelete = await Post.deleteOne({ id: req.params.id })
+  if (postToDelete.deletedCount == 0) {
+    throw new BadRequestError('user not exist')
+  }
+  res.status(StatusCodes.OK).json()
 }
 
 export { getPosts, createNewPost, getPostById, deletePostByID }
