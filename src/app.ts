119c5532cb27916ff74c18c44cc734149879f8a8
kartowka <antonfley@gmail.com>
2022-04-07 16:37:30 +0300

    swagger imports and impl.

--- src/app.ts
+++ src/app.ts
@@ -9,21 +9,30 @@ import 'express-async-errors'
 import dotenv from 'dotenv'
 dotenv.config()
 
-// * routers
-// * app.use
 import bodyParser from 'body-parser'
+// * swaggerUI
+import swaggerUI from 'swagger-ui-express'
+
+import swaggerConnection from './documentation/swagger'
+app.use(
+  '/api-docs',
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerConnection(process.env.NODE_ENV, process.env.PORT))
+)
 
 // * middleware
 import error_handler_middleware from './middleware/error-handler'
 import not_found_middleware from './middleware/not_found'
+// * routers
 import authRouter from './routes/auth'
 import postRouter from './routes/post'
-
+// * app.use
 app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }))
 app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.json({ msg: 'Welcome!' })
 })
+
 app.use('/api/auth', authRouter)
 app.use('/api/post', postRouter)
 app.use(not_found_middleware)
