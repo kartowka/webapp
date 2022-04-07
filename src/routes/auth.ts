a59d671fbc6adc72f36e77d60a121dc1da06a223
kartowka <antonfley@gmail.com>
2022-04-07 11:20:45 +0300

    reformatted document after prettier print_width change

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 const router = express.Router()
-import { deleteByID, login, register } from '../controllers/auth'
+import { deleteByID, login, logout, register } from '../controllers/auth'
 
 router.route('/register').post(register)
 router.route('/login').post(login)
 router.route('/user/:id').delete(deleteByID)
+router.route('/logout').delete(logout)
 
 export default router
--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,15 +1,12 @@
 import express from 'express'
-const router = express.Router()
-import {
-  createNewPost,
-  deletePostByID,
-  getPostById,
-  getPosts,
-} from '../controllers/post'
 
-router.route('/').post(createNewPost)
+import authenticationMiddleware from '../middleware/authentication'
+const router = express.Router()
+import { createNewPost, deletePostByID, getPostById, getPosts } from '../controllers/post'
+
+router.route('/').post(authenticationMiddleware, createNewPost)
 router.route('/').get(getPosts)
 router.route('/:id').get(getPostById)
-router.route('/:id').delete(deletePostByID)
+router.route('/:id').delete(authenticationMiddleware, deletePostByID)
 
 export default router
