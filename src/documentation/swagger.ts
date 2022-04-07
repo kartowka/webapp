77920d4a9c9667644b3363d4747aceae52f21c06
kartowka <antonfley@gmail.com>
2022-04-07 16:37:54 +0300

    swagger configuration file

---
+++ src/documentation/swagger.ts
@@ -0,0 +1,20 @@
+import swaggerJsDoc from 'swagger-jsdoc'
+
+const swaggerConnection = (env: string, port: string) => {
+  if (env == 'development') {
+    const options = {
+      definition: {
+        openapi: '3.0.0',
+        info: {
+          title: 'Library API',
+          version: '1.0.0',
+          description: 'A simple Express Library API',
+        },
+        servers: [{ url: 'http://localhost:' + process.env.PORT }],
+      },
+      apis: ['./src/routes/*.ts'],
+    }
+    return swaggerJsDoc(options)
+  }
+}
+export default swaggerConnection
