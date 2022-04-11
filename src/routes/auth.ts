2c29f9b0efea86baea92777c8c167060899b7b27
kartowka <antonfley@gmail.com>
2022-04-11 12:09:33 +0300

    added logout using the refreshToken instead of simple id

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+
 const router = express.Router()
 import { deleteByID, login, logout, register, renewToken } from '../controllers/auth'
 import renewAuthenticationMiddleware from '../middleware/renew-authentication'
@@ -148,42 +149,20 @@ router.route('/user/:id').delete(deleteByID)
  *               refreshToken: 'long long auto-generated string'
  *               token: 'long long auto-generated string'
  */
-router.route('/logout').delete(logout)
+router.route('/logout').delete(renewAuthenticationMiddleware, logout)
 /**
  * @swagger
  * /api/auth/logout:
  *   delete:
- *     summary: logout
+ *     summary: logout a user
  *     tags: [Auth]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/User'
+ *     description: need to provide the refresh token in the auth header
+ *     security:
+ *       - bearerAuth: []
  *     responses:
  *       200:
- *         description: logout
- *         content:
- *           application/json:
- *             schema:
- *               _id:
- *                 type: string
- *                 description: auto-generated id
- *               token:
- *                 type: string
- *                 description: auto-generated accessToken
- *               refreshToken:
- *                 type: string
- *                 description: auto-generated refreshToken
- *               email:
- *                 type: string
- *                 description: user`s email address
- *             example:
- *               _id: 'auto-generated user id'
- *               email: 'bob@gmail.com'
- *               refreshToken: 'long long auto-generated string'
- *               token: 'long long auto-generated string'
+ *         description: logout completed successfully
+ *
  */
 router.route('/token').post(renewAuthenticationMiddleware, renewToken)
 /**
