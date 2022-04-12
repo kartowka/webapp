aa7f41af1d14b0a130dbe5c8e93b2ffd9b8f24c7
kartowka <antonfley@gmail.com>
2022-04-12 15:12:07 +0300

    added case on logging destionation in case output to file,also async output

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -4,9 +4,10 @@ const logger = pino({
   transport: {
     target: 'pino-pretty',
     options: {
-      destination: 1,
+      destination: 1, //'./src/utils/serverlog.log',
       translateTime: 'SYS:dd-mm-yyyy HH:MM:ss',
       ignore: 'pid,hostname',
+      sync: false,
     },
   },
 })
