207de6eb696f4459c34ce2a5820dfbf2e3ba6c74
kartowka <antonfley@gmail.com>
2022-04-13 12:30:16 +0300

    added ims testing, Note: redis-server process should be on for this test to work

--- src/tests/socket.test.ts
+++ src/tests/socket.test.ts
@@ -5,23 +5,31 @@ import request from 'supertest'
 
 import User from '../models/User'
 import server from '../server'
-import logger from '../utils/logger'
+import { redisClientDisconnect } from '../socket-server.js'
 
 type socketUserModel = {
+  _id: string
   email: string
   password: string
   accessToken: string
   clientSocket: Socket
 }
+type InstantMessage = {
+  body: string
+  receiver: string
+  sender: string
+}
 
 const users: socketUserModel[] = [
   {
+    _id: '',
     email: 'user1@socket.io',
     password: '123456',
     accessToken: '',
     clientSocket: null,
   },
   {
+    _id: '',
     email: 'user2@socket.io',
     password: '123456',
     accessToken: '',
@@ -44,15 +52,23 @@ afterAll(async () => {
   await User.deleteMany({ email: { $regex: '@socket.io' } })
   await serverCleanup()
   mongoose.connection.close()
+  await redisClientDisconnect()
 })
 
 describe('Socket.IO connection testing', () => {
   const registerUser = async (user: socketUserModel) => {
-    const response = await request(server)
+    await request(server)
       .post('/api/auth/register')
       .send({ email: user.email, password: user.password })
       .expect(StatusCodes.CREATED)
+  }
+  const loginUser = async (user: socketUserModel) => {
+    const response = await request(server)
+      .post('/api/auth/login')
+      .send({ email: user.email, password: user.password })
+      .expect(StatusCodes.OK)
     user.accessToken = response.body.token
+    user._id = response.body.user._id
   }
   const socketClientConnectionOpen = (user: socketUserModel, done: jest.DoneCallback) => {
     user.clientSocket = Client('http://localhost:' + process.env.PORT, {
@@ -68,6 +84,10 @@ describe('Socket.IO connection testing', () => {
     await registerUser(users[0])
     await registerUser(users[1])
   })
+  it('should login users', async () => {
+    await loginUser(users[0])
+    await loginUser(users[1])
+  })
   it('should open connection for users', done => {
     socketClientConnectionOpen(users[0], done)
     socketClientConnectionOpen(users[1], done)
@@ -78,5 +98,15 @@ describe('Socket.IO connection testing', () => {
       done()
     })
     users[0].clientSocket.emit('common:echo', 'echo message')
+  })
+  it('should test ims', done => {
+    const msg = { body: 'this is a test message', receiver: users[1]._id }
+    users[0].clientSocket.emit('ims:instantMsg', msg)
+
+    users[1].clientSocket.on('ims:instantMsg', (rcMsg: InstantMessage) => {
+      expect(rcMsg.body).toBe(msg.body)
+      expect(rcMsg.sender).toBe(users[0]._id)
+      done()
+    })
   })
 })
