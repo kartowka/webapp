90b532a7e5f6f161550829d53991a0592e86859c
kartowka <antonfley@gmail.com>
2022-05-29 19:26:40 +0300

    homescreen page

---
+++ client/src/screens/home/HomeScreen.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react'
+import { View, FlatList, Text } from 'react-native'
+import { HomeScreenWrapper } from '@assets/styles'
+import StudentDetails from '@screens/student/StudentDetails'
+import { getStudents } from '@model/student'
+
+const HomeScreen: FC<{ navigation: any; route: any }> = ({ navigation, route }) => {
+	const onItemClick = (itemID: string) => {
+		navigation.navigate('Details', { studentID: itemID })
+	}
+	const studentsDetails = getStudents()
+	return (
+		<View style={HomeScreenWrapper.background}>
+			<FlatList
+				data={studentsDetails}
+				keyExtractor={(item) => item.id.toString()}
+				renderItem={({ item }) => <StudentDetails id={item.id} name={item.name} onItemSelect={onItemClick}></StudentDetails>}
+			></FlatList>
+		</View>
+	)
+}
+
+export default HomeScreen
