1e201981fde4fd0e413dbdb699c0ff0eda8cd922
kartowka <antonfley@gmail.com>
2022-05-29 19:24:02 +0300

    student model WIP

---
+++ client/src/model/student.tsx
@@ -0,0 +1,22 @@
+type student = {
+	id: string
+	name: string
+}
+const students: Array<student> = [
+	{ name: 'Eitan', id: '203192331' },
+	{ name: 'Eitan2', id: '203192332' },
+	{ name: 'Eitan3', id: '203192333' },
+	{ name: 'Eitan4', id: '203192334' },
+]
+
+const getStudents = () => {
+	return students
+}
+const getStudent = (id: string) => {
+	return students
+}
+const addStudent = (student: student) => {
+	students.push(student)
+}
+
+export { getStudents, getStudent, addStudent, student }
