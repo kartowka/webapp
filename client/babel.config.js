9a7d1b0eefccdc64edf444cad5647d71ca228a78
kartowka <antonfley@gmail.com>
2022-05-29 19:20:37 +0300

    absolute path added

---
+++ client/babel.config.js
@@ -0,0 +1,24 @@
+module.exports = function (api) {
+	api.cache(true)
+	return {
+		presets: ['babel-preset-expo'],
+		plugins: [
+			[
+				'module-resolver',
+				{
+					root: ['./src'],
+					extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json', '.png'],
+					alias: {
+						'@components': './src/components',
+						'@screens': './src/screens',
+						'@assets': './src/assets',
+						'@images': './src/assets/images',
+						'@styles': './src/assets/styles',
+						'@model': './src/model',
+						'@constants': './src/constants',
+					},
+				},
+			],
+		],
+	}
+}
