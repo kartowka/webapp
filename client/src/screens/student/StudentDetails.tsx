1476ff1577a57f360c25fa4f71f5bd073378fc29
kartowka <antonfley@gmail.com>
2022-05-29 19:26:16 +0300

    student details page

---
+++ client/src/screens/student/StudentDetails.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { View, Image, Text, TouchableOpacity } from 'react-native'
+import { StudentDetailsWrapper } from '@styles/index'
+
+const StudentDetails: React.FC<{ id: string; name: string; onItemSelect: (itemID: string) => void }> = ({ name, id, onItemSelect }) => {
+	return (
+		<TouchableOpacity
+			onPress={() => {
+				onItemSelect(id)
+			}}
+		>
+			<View style={StudentDetailsWrapper.container}>
+				<Image source={require('@images/anonymous-icon-2.png')} style={StudentDetailsWrapper.avatar}></Image>
+				<View style={StudentDetailsWrapper.textContainer}>
+					<Text style={StudentDetailsWrapper.studentName}>{name}</Text>
+					<Text style={StudentDetailsWrapper.studentID}>{id}</Text>
+				</View>
+			</View>
+		</TouchableOpacity>
+	)
+}
+
+export default StudentDetails
